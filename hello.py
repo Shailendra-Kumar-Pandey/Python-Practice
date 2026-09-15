1e4b77044bd6db877d2eeb4f618e7b28e1992c74
shailendra-kumar-pandey <shailendrakr2010@gmail.com>
2026-09-15 18:21:50 +0530

    hello file

---
+++ hello.py
@@ -0,0 +1,8 @@
+print("Hello Shailendra!")
+
+# this is a simple Python script that prints a greeting message and then iterates through a loop five times, printing the current iteration number each time.
+# for i in range(5):
+#     print(f"Iteration {i + 1}") 
+
+
+
