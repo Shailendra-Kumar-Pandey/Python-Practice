e682855db441e4985e212720ea4105eb1c45436a
shailendra-kumar-pandey <shailendrakr2010@gmail.com>
2026-09-17 12:05:07 +0530

    countdown

--- table.py
+++ table.py
@@ -2,3 +2,11 @@ number = int(input("Which number table do you want: "))
 
 for i in range(1, 11):
     print(number, "*", i, "=", number * i)
+
+
+
+count = int(input("Enter the number: "))
+
+while count > 0:
+    print(count)
+    count -= 1
